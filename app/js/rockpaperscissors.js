b39a666d1b1b2c78b570e617f7bc7bd17b120532
cj <christopher.dobbins@alumni.vanderbilt.edu>
2015-07-31 02:17:29 -0400

    "finished 7/31 at 2:17 am"

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -33,7 +33,7 @@ function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
@@ -41,7 +41,20 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+
+//     console.log("You played: " + playerMove + "\n" + "Computer played: " + computerMove);
+     if (playerMove === computerMove){
+    winner = "tie";
+  }
+  else if ((playerMove === "rock"&& computerMove === "scissors") || 
+      (playerMove === "paper" && computerMove === "rock") ||
+       (playerMove === "scissors" && computerMove === "paper")) {
+    winner = "player";
+  }
+  
+  else {
+      winner = "computer";
+  }
     return winner;
 }
 
@@ -50,7 +63,17 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    for (var i = 0; i < 5; i++){
+       var winner = getWinner(getPlayerMove(), getComputerMove());
+       if (winner === "player"){
+          playerWins++;
+       }
+       else if (winner === "computer"){
+          computerWins++;
+       }
+   }
     return [playerWins, computerWins];
 }
+
+console.log(playToFive());
 
